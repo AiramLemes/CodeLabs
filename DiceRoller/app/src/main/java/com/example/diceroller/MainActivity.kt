4f602a2471eee48b4f3f049d3adecb03cf043338
AiramLemes <90828450+AiramLemes@users.noreply.github.com>
2022-10-12 17:10:34 +0100

    CodeLab 1.2 Buttons
    Roll and Count Up Buttons. Text changes when buttons are clicked.

---
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.text.isDigitsOnly
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener { rollDice() }
+
+        val countButton: Button = findViewById(R.id.count_button)
+        countButton.setOnClickListener { countUp() }
+
+    }
+
+    private fun countUp() {
+        val resultText: TextView = findViewById(R.id.result_text)
+        var text = resultText.text.toString()
+
+        if (text.toString().isDigitsOnly()) {
+
+            if (text.toInt() < 6) {
+                resultText.text = (text.toInt() + 1).toString()
+            }
+
+        }
+        else {
+            resultText.text = "1"
+        }
+    }
+
+    private fun rollDice() {
+        /*Toast.makeText(this, "button clicked",
+        Toast.LENGTH_SHORT).show()*/
+        val randomInt = (1..6).random()
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = randomInt.toString()
+    }
+}
