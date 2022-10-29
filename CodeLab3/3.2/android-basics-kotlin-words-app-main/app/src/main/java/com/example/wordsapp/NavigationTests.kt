a9664558a8ae8b41f1135c9a014bc1484d17dc16
AiramLemes <90828450+AiramLemes@users.noreply.github.com>
2022-10-29 11:13:21 +0100

    Codelab 3.2 Test Navigation

---
+++ CodeLab3/3.2/android-basics-kotlin-words-app-main/app/src/main/java/com/example/wordsapp/NavigationTests.kt
@@ -0,0 +1,21 @@
+package com.example.wordsapp
+
+
+import androidx.navigation.testing.TestNavHostController
+import androidx.test.core.app.ApplicationProvider
+import org.junit.Test
+
+class NavigationTests {
+
+
+    @Test
+    fun navigate_to_words_nav_component() {
+
+        val navController = TestNavHostController(
+            ApplicationProvider.getApplicationContext()
+        )
+
+    }
+
+
+}
